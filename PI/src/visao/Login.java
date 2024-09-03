935c0162183b845a86e8d7974873242fa214ac2f
carlos <soaresecarlos15@gmail.com>
2024-09-03 17:24:21 -0300

    Img da primeira tela

--- PI/src/visao/Login.java
+++ PI/src/visao/Login.java
@@ -6,11 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.FlowLayout;
-import java.awt.Color;
 import javax.swing.JLabel;
-import java.awt.Font;
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class Login extends JFrame {
 
@@ -38,7 +38,7 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(500, 500, 800, 500);
+		setBounds(700, 700, 1000, 700); 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -46,36 +46,36 @@ public class Login extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(56, 55, 70));
-		FlowLayout flowLayout_1 = (FlowLayout) panel.getLayout();
-		flowLayout_1.setVgap(30);
-		contentPane.add(panel, BorderLayout.NORTH);
+		contentPane.add(panel, BorderLayout.SOUTH);
 		
-		JLabel lblFazerLogin = new JLabel("Fazer Login");
-		lblFazerLogin.setFont(new Font("SimSun", Font.PLAIN, 20));
-		lblFazerLogin.setForeground(new Color(255, 255, 255));
-		panel.add(lblFazerLogin);
+		JLabel imgOndinha = new JLabel("");
+		
+		ImageIcon originalIcon = new ImageIcon(Login.class.getResource("/Imagens/Ondinha23.png"));
+		Image image = originalIcon.getImage(); // Obtenha a imagem do ImageIcon
+		Image newImage = image.getScaledInstance(1000, 250, Image.SCALE_SMOOTH); // Redimensione a imagem
+		
+		ImageIcon resizedIcon = new ImageIcon(newImage);
+		imgOndinha.setIcon(resizedIcon);
+		panel.add(imgOndinha);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(56, 55, 70));
-		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
-		flowLayout.setVgap(0);
 		contentPane.add(panel_1, BorderLayout.WEST);
 		
-
-		JLabel LblIcon = new JLabel("");
-		LblIcon.setBackground(new Color(69, 68, 89));
+		JLabel imgLogo = new JLabel("");
+		ImageIcon originalIconLogo = new ImageIcon(Login.class.getResource("/Imagens/Logo2.png"));
+		Image imageLogo = originalIconLogo.getImage(); // Obtenha a imagem do ImageIcon
+		Image NovaLogo = imageLogo.getScaledInstance(100, 100, Image.SCALE_SMOOTH); // Redimensione a imagem
+		ImageIcon ImgRedimencionada = new ImageIcon(NovaLogo);
+		imgLogo.setIcon(ImgRedimencionada);
 		
-		LblIcon.setIcon(new ImageIcon(Login.class.getResource("/Imagens/telas.png")));
-		panel_1.add(LblIcon);
 		
-		JPanel panel_2 = new JPanel();
-		contentPane.add(panel_2, BorderLayout.CENTER);
-		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel LBLMK = new JLabel("SVGEM");
-		LBLMK.setFont(new Font("SimSun", Font.PLAIN, 98));
-		panel_2.add(LBLMK);
+		imgLogo.setVerticalAlignment(SwingConstants.BOTTOM);
+		
+		panel_1.add(imgLogo);
+		
+		
+		
 	}
 
 }
