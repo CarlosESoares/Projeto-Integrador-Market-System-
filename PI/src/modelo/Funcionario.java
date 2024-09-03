d69fabe4705b5023128972bd05585b2b1d0252bb
Arthur <arthur.fr.sc@gmail.com>
2024-09-03 09:06:57 -0300

    classe funcionario

--- PI/src/modelo/Funcionario.java
+++ PI/src/modelo/Funcionario.java
@@ -2,8 +2,60 @@ package modelo;
 
 public class Funcionario {
 private String NomeFuncionario;
+private String SobrenomeFuncionario;
 private int IdFuncionario;
 private String SenhaFuncionario;
-private String
+private float TelefoneFuncionario;
+private String AreaTrabalho;
+private String Endereco;
+private double Salario;
+public String getNomeFuncionario() {
+	return NomeFuncionario;
+}
+public void setNomeFuncionario(String nomeFuncionario) {
+	NomeFuncionario = nomeFuncionario;
+}
+public String getSobrenomeFuncionario() {
+	return SobrenomeFuncionario;
+}
+public void setSobrenomeFuncionario(String sobrenomeFuncionario) {
+	SobrenomeFuncionario = sobrenomeFuncionario;
+}
+public int getIdFuncionario() {
+	return IdFuncionario;
+}
+public void setIdFuncionario(int idFuncionario) {
+	IdFuncionario = idFuncionario;
+}
+public String getSenhaFuncionario() {
+	return SenhaFuncionario;
+}
+public void setSenhaFuncionario(String senhaFuncionario) {
+	SenhaFuncionario = senhaFuncionario;
+}
+public float getTelefoneFuncionario() {
+	return TelefoneFuncionario;
+}
+public void setTelefoneFuncionario(float telefoneFuncionario) {
+	TelefoneFuncionario = telefoneFuncionario;
+}
+public String getAreaTrabalho() {
+	return AreaTrabalho;
+}
+public void setAreaTrabalho(String areaTrabalho) {
+	AreaTrabalho = areaTrabalho;
+}
+public String getEndereco() {
+	return Endereco;
+}
+public void setEndereco(String endereco) {
+	Endereco = endereco;
+}
+public double getSalario() {
+	return Salario;
+}
+public void setSalario(double salario) {
+	Salario = salario;
+}
 
 }
