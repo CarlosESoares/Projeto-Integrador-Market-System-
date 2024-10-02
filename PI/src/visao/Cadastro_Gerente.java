16d6bb70c6b2b081e6f4b106be19e3c2b28f3715
Lucas Oliveira <lucas.l28@aluno.ifsc.edu.br>
2024-10-02 11:07:23 -0300

    Partes desnecessárias do código

--- PI/src/visao/Cadastro_Gerente.java
+++ PI/src/visao/Cadastro_Gerente.java
@@ -12,14 +12,8 @@ import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
-import javax.swing.JTextField;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -30,9 +24,6 @@ public class Cadastro_Gerente extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField TextNome;
-	private JTextField TextCpf;
-	private JTextField TextSenha;
 
 	/**
 	 * Launch the application.
