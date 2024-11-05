f44e73734be23cfa2fad5851a2dbaa9a249cfcfb
Carlos <carlos.s2006@aluno.ifsc.edu.br>
2024-11-05 10:20:53 -0300

    Gerente

--- PI/src/Controle/ControllerGerente.java
+++ PI/src/Controle/ControllerGerente.java
@@ -1,16 +1,28 @@
 package Controle;
 
-import visao.Login;
+import visao.Cadastro_Gerente;
+import visao.TelaGeFunci;
 import visao.telaInicial;
 
 public class ControllerGerente {
 
-	public void AbrirTelaGerente() {
+	public void TelaInicial() {
 
 		telaInicial tela = new telaInicial();
 		
 		tela.setVisible(true);
 		
 	}
+	public void FUnfo() {
+
+		TelaGeFunci Abrir = new TelaGeFunci();
+		Abrir.setVisible(true);
+
+	}
+	public void TelaCadastro() {
+		Cadastro_Gerente abrir = new Cadastro_Gerente();
+		abrir.setVisible(true);
+	}
+	
 
 }
--- PI/src/visao/TelaGerente.java
+++ PI/src/visao/TelaGerente.java
@@ -1,27 +1,24 @@
 package visao;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.Image;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.SpringLayout;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
 
-import Controle.ControllerTelaCliente;
-import net.miginfocom.swing.MigLayout;
-import java.awt.BorderLayout;
+import Controle.ControllerGerente;
 
 public class TelaGerente extends JFrame {
-
+	private static TelaGerente frame = new TelaGerente();
 	private JPanel contentPane;
 
 	/**
@@ -30,8 +27,8 @@ public class TelaGerente extends JFrame {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+
 				try {
-					TelaGerente frame = new TelaGerente();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -40,72 +37,103 @@ public class TelaGerente extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public TelaGerente() {
+
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1042, 536);
+		setBounds(0, 0,500,500); 
+		setLocationRelativeTo(null);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		
-		ImageIcon originalIcon = new ImageIcon(Login.class.getResource("/Imagens/Ondinha23.png"));
-		Image image = originalIcon.getImage(); // Obtenha a imagem do ImageIcon
-		Image newImage = image.getScaledInstance(1200, 200, Image.SCALE_SMOOTH); // Redimensione a imagem
-		
-		ImageIcon resizedIcon = new ImageIcon(newImage);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(192, 192, 192));
-		contentPane.add(panel, BorderLayout.WEST);
-		panel.setLayout(new MigLayout("", "[]", "[]"));
-		ImageIcon originalIconLogo = new ImageIcon(Login.class.getResource("/Imagens/Logo2.png"));
-		Image imageLogo = originalIconLogo.getImage(); // Obtenha a imagem do ImageIcon
-		Image NovaLogo = imageLogo.getScaledInstance(100, 100, Image.SCALE_SMOOTH); // Redimensione a imagem
-		ImageIcon ImgRedimencionada = new ImageIcon(NovaLogo);
+		JPanel panel_2 = new JPanel();
 		
-		RoundedButton imgLogo = new RoundedButton("",1,1);
-		imgLogo.setBackground(new Color(192, 192, 192));
-		panel.add(imgLogo, "cell 0 0");
-		imgLogo.addActionListener(new ActionListener() {
+		contentPane.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(null);
+		
+		
+		
+		
+		
+		JLabel lblNewLabel = new JLabel("Gerentes");
+		lblNewLabel.setBounds(154, 22, 179, 47);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 40));
+		panel_2.add(lblNewLabel);
+
+		
+		RoundedButton Btnfuncionarios = new RoundedButton("Entrar",30,30);
+		Btnfuncionarios.setText("Funcionarios");
+		Btnfuncionarios.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ControllerTelaCliente abrir = new ControllerTelaCliente();
-				abrir.AbrirTelaInicial();
-				dispose();
+				
+				ControllerGerente tela = new ControllerGerente();
+				
+				
+				frame.dispose();
+				tela.TelaCadastro();
+				
+
 			}
 		});
-		imgLogo.setIcon(ImgRedimencionada);
-		
-		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, BorderLayout.CENTER);
-		panel_1.setLayout(new MigLayout("", "[][][][][][][][][]", "[][][][][][][][][]"));
-		
-		RoundedButton btnNewButton = new RoundedButton("New button",30,30);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
+		Btnfuncionarios.addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent e) {
+		    	Btnfuncionarios.setBackground(Color.LIGHT_GRAY);
+		    }
+		    public void mouseExited(MouseEvent e) {
+		    	Btnfuncionarios.setBackground(Color.RED);
+		    }
 		});
-		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setBackground(new Color(255, 0, 0));
-		panel_1.add(btnNewButton, "cell 2 2");
+		Btnfuncionarios.setBounds(154, 144, 179, 32);
+	
 		
-		RoundedButton btnNewButton_1 = new RoundedButton("New button",30,30);
-		btnNewButton_1.setBackground(new Color(255, 0, 0))
-		;
-		btnNewButton_1.setForeground(new Color(255, 255, 255));
-
-		panel_1.add(btnNewButton_1, "cell 4 2");
+		Btnfuncionarios.setBackground(new Color(255, 0, 0));
+		Btnfuncionarios.setForeground(new Color(255, 255, 255));
+		Btnfuncionarios.setFont(new Font("Arial", Font.BOLD, 20));
+		Btnfuncionarios.setBackground(new Color(255, 0, 0));
+		panel_2.add(Btnfuncionarios);
 		
-		RoundedButton btnNewButton_2 = new RoundedButton("New button",30,30);
-		btnNewButton_2.setBackground(new Color(255, 0, 0));
-		btnNewButton_2.setForeground(new Color(255, 255, 255));
-
-		panel_1.add(btnNewButton_2, "cell 7 2");
+		JLabel imgOndinha = new JLabel("");
+		imgOndinha.setBounds(-81, 305, 729, 342);
+		panel_2.add(imgOndinha);
+		imgOndinha.setIcon(new ImageIcon(Login.class.getResource("/Imagens/Ondinha23.png")));
+		imgOndinha.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		imgOndinha.setBackground(Color.LIGHT_GRAY);
 		
+		RoundedButton Btnvendas = new RoundedButton("Entrar", 30, 30);
+		Btnvendas.setText("Vendas");
+		Btnvendas.setForeground(Color.WHITE);
+		Btnvendas.setFont(new Font("Arial", Font.BOLD, 20));
+		Btnvendas.setBackground(Color.RED);
+		Btnvendas.setBounds(154, 198, 179, 32);
+		panel_2.add(Btnvendas);
+		Btnvendas.addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent e) {
+		    	Btnvendas.setBackground(Color.LIGHT_GRAY);
+		    }
+		    public void mouseExited(MouseEvent e) {
+		    	Btnvendas.setBackground(Color.RED);
+		    }
+		});
 		
-	}
+		RoundedButton estoqueBtn = new RoundedButton("Entrar", 30, 30);
+		estoqueBtn.setText("Estoque");
+		estoqueBtn.setForeground(Color.WHITE);
+		estoqueBtn.setFont(new Font("Arial", Font.BOLD, 20));
+		estoqueBtn.setBackground(Color.RED);
+		estoqueBtn.setBounds(154, 247, 179, 32);
+		panel_2.add(estoqueBtn);
+		estoqueBtn.addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent e) {
+		    	estoqueBtn.setBackground(Color.LIGHT_GRAY);
+		    }
+		    public void mouseExited(MouseEvent e) {
+		    	estoqueBtn.setBackground(Color.RED);
+		    }
+		});
+		}
 }
